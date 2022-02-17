297972a9fc16ee01b1e83f4672ab06c500cda6b5
Hodaya Siman Tov <hodaysi123@gmail.com>
2022-02-17 22:27:50 +0200

    123

--- smartrack/app/src/main/java/com/smartrack/smartrack/ui/futureTrip/ListTripInDayFragment.java
+++ smartrack/app/src/main/java/com/smartrack/smartrack/ui/futureTrip/ListTripInDayFragment.java
@@ -14,7 +14,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.smartrack.smartrack.Model.PlacePlanning;
@@ -24,55 +26,41 @@ import com.smartrack.smartrack.ui.planTrip.PlacesListFragmentArgs;
 import com.smartrack.smartrack.ui.planTrip.PlacesListFragmentDirections;
 import com.squareup.picasso.Picasso;
 
-import java.util.ArrayList;
 
-
-public class ListDayInTripFragment extends Fragment {
+public class ListTripInDayFragment extends Fragment {
+    TextView name,location;
+    ImageView imagev;
+    RatingBar rating;
     PlacePlanning[] arrayPlaces;
-    Integer tripDays;
     ListView listViewPlaces;
     MyAdapter adapter;
-    int [] arrDays;
-    TextView numDay;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_list_day_in_trip, container, false);
-
-        arrayPlaces = ListDayInTripFragmentArgs.fromBundle(getArguments()).getArrTripsDay();
-        tripDays=  ListDayInTripFragmentArgs.fromBundle(getArguments()).getTripDays();
-        arrDays = new int[tripDays];
-
-        listViewPlaces =view.findViewById(R.id.fragment_list_day_in_trip_list_view);
-        ArrayList<ArrayList<PlacePlanning>> array_place_days_planing = new ArrayList<>();
-        for(int k=0; k<tripDays;++k){
-            array_place_days_planing.add(new ArrayList<>());
-        }
-        for (int j=0; j<arrayPlaces.length;++j){
-            array_place_days_planing.get(arrayPlaces[j].getDay_in_trip()-1).add(arrayPlaces[j]);
-        }
-
+        View view = inflater.inflate(R.layout.fragment_list_trip_in_day, container, false);
+        listViewPlaces =view.findViewById(R.id.fragment_list_trip_in_day_list_view);
+        arrayPlaces =  ListTripInDayFragmentArgs.fromBundle(getArguments()).getArrTrip();
         adapter=new MyAdapter();
         listViewPlaces.setAdapter(adapter);
         listViewPlaces.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int i, long l) {
-                PlacePlanning[] newArrayPlaces = new PlacePlanning[array_place_days_planing.get(i).size()];
-                array_place_days_planing.get(i).toArray(newArrayPlaces);
-                ListDayInTripFragmentDirections.ActionListDayInTripFragmentToListTripInDayFragment action =  ListDayInTripFragmentDirections.actionListDayInTripFragmentToListTripInDayFragment(newArrayPlaces);
-                Navigation.findNavController(view).navigate( action);
+                Log.d("TAG","post id"+i);
+//                PlacesListFragmentDirections.ActionPlacesListFragmentToPlaceDetailsFragment action=PlacesListFragmentDirections.actionPlacesListFragmentToPlaceDetailsFragment(arrayPlaces[i]);
+//                Navigation.findNavController(view).navigate( action);
             }
         });
         return view;
     }
+
     class MyAdapter extends BaseAdapter {
         @Override
         public int getCount() {
-            if (arrDays== null) {
+            if (arrayPlaces== null) {
                 return 0;
             } else {
-                return arrDays.length;
+                return arrayPlaces.length;
             }
         }
 
@@ -90,12 +78,29 @@ public class ListDayInTripFragment extends Fragment {
         public View getView(int i, View view, ViewGroup viewGroup) {
             if (view == null) {
                 LayoutInflater inflater = getLayoutInflater();
-                view = inflater.inflate(R.layout.day_list_row, null);
+                view = inflater.inflate(R.layout.trip_in_day_row, null);
             } else {
 
             }
-            numDay = view.findViewById(R.id.day_list_row_textview);
-            numDay.setText("Day "+(i+1));
+            PlacePlanning place=arrayPlaces[i];
+
+            name = view.findViewById(R.id.trip_in_day_row_name_trip);
+            imagev = view.findViewById(R.id.trip_in_day_row_image);
+            name.setText(place.getPlaceName());
+            imagev.setTag(place.getPlaceImgUrl());
+            rating=view.findViewById(R.id.trip_in_day_row_rating);
+            rating.setRating(place.getPlaceRating());
+            Drawable drawable = rating.getProgressDrawable();
+            drawable.setColorFilter(Color.parseColor("#FDC313"), PorterDuff.Mode.SRC_ATOP);
+            if (place.getPlaceImgUrl() != null && place.getPlaceImgUrl() != "") {
+                if (place.getPlaceImgUrl() == imagev.getTag()) {
+                    Picasso.get().load(place.getPlaceImgUrl()).into(imagev);
+                }
+            } else {
+
+
+            }
+
             return view;
         }
 
